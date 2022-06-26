3d6bc7b3846c08fd3d9f5002c6e2dbb06bee7539
durgachanakya <81852237+durgachanakya@users.noreply.github.com>
2022-06-26 14:54:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,5 +17,16 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        leftwristx = results[0].pose.leftWrist.x;
+    rightwristx = results[0].pose.rightWrist.x;
+    difference = floor(leftwristx - rightwristx);
     }
+}
+
+function draw(){
+    background("black");
+    fill("white");
+    textSize(difference);
+    text("Chanakya" , 100,100);
+    
 }
